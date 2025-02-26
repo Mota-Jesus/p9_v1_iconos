db71a2bf5736389d68bb98b391c0a6caf9cf8526
JESUS_MOTA_BUSTILLOS <a.22308051281275@cbtis128.edu.mx>
2025-02-26 20:10:05 +0000

    precticas iconos

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,62 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MisIconosApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconosApp extends StatelessWidget {
+  const MisIconosApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Jesus Mota"),
+          titleTextStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+          centerTitle: true,
+          backgroundColor: const Color(0xff326074),
+        ),
+        body: const Column(
+          children: <Widget>[
+            Text(
+              "Jesus Mota Bustillos Mat: 22308051281275 Gpo: 6ºJ",
+              style: TextStyle(fontSize: 20, color: Colors.amber),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Icon(
+                  Icons.thumb_down_alt_sharp,
+                  color: Colors.pink,
+                  size: 40.0,
+                  semanticLabel: 'Text to announce in accessibility modes',
+                ),
+                Icon(
+                  Icons.audiotrack,
+                  color: Colors.indigo,
+                  size: 40.0,
+                ),
+                Icon(
+                  Icons.beach_access,
+                  color: Colors.lime,
+                  size: 40.0,
+                ),
+                Icon(
+                  Icons.airline_stops_sharp,
+                  color: Color(0xffe1710a),
+                  size: 40.0,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
-  }
-}
+  } //fin widgets
+} //fin de clase mis iconos app
